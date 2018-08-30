80dd403c8f4f61d36dc9845bffdbb891e585a959
Mike Burr <mburr@unintuitive.org>
2018-08-29 17:45:55 -0700

    be smarter about column "types". rest.

--- mutils/rest.py
+++ mutils/rest.py
@@ -1,40 +1,20 @@
 # -*- mode: python; coding: utf-8 -*-
 
-import os
-import sys
-import time
-import tempfile
-import re
-import requests
-import datetime
-import pytz
-import json
 import urllib
-from requests.auth import HTTPBasicAuth
+import requests
 import logging
-import logging.handlers
-import lockfile
-from dateutil.parser import parse as parse_dt
-
-import daemon
-import daemon.pidfile
-
-import db
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def rr(scheme, netloc, url, params='', query='', fragment='', headers={}, auth=None):
-    """request+response -- given a location and query, just return the (json) data.
+def get_json(url, headers={}, auth=None):
+    """request+response -- url, just return the (json) data.
     Handle errors appropriately.
     """
 
-    location = '/' + location.lstrip('/')
-
-    url = urllib.parse.urlparse(urllib.parse.urlunparse(
-        (scheme, netloc, url, params, query, fragment)
-    )).geturl()
+    if not isinstance(url, str):
+        url = url.geturl()
 
     logger.debug('trying to get {}'.format(url))
     response = requests.get(url, auth=auth)
--- mutils/simple_alchemy.py
+++ mutils/simple_alchemy.py
@@ -4,36 +4,48 @@
 
 import os
 import atexit
+import inspect
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.types import to_instance, TypeEngine
 
 Base = declarative_base()
 
 
-def get_table_class(table_name, schema):
+def get_table_class(table_name, schema, include_id=True):
     """`table_name` (str) is used for both the python class name and the `__tablename__`.
 
     `schema` is a sequence of (`name`, `type`) tuples, `name` being the column name `type`
     being the SQLAlchemy type class to use.
     """
 
-    attrs = dict(
-        __tablename__=table_name,
-        id=Column(Integer, primary_key=True))
+    attrs = {'__tablename__': table_name}
+
+    if include_id:
+        attrs['id'] = Column(Integer, primary_key=True)
 
     for row in schema:
+        kwargs = {}
+
         if len(row) == 1 or not row[1]:
             column = row[0]
             type_ = String
+            args = (type_,)
         elif isinstance(row, str):
             column = row
             type_ = String
-        else:
+            args = (type_,)
+        elif inspect.isclass(row[1]) and issubclass(row[1], TypeEngine):
             column, type_ = row
-        attrs[column] = Column(type_)
+            args = (type_,)
+        else:
+            column = row[0]
+            args, kwargs = row[1]
+
+        attrs[column] = Column(*args, **kwargs)
 
     return type(table_name, (Base,), attrs)
 
