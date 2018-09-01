ef31916361a3647a5fb37be13d9d10844b2c0c3f
Mike Burr <mburr@unintuitive.org>
2018-09-01 08:04:57 -0700

    meta_setup bad idea

--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
 # -*- mode: python; coding: utf-8 -*-
 
-from mutil.pypi import meta_setup
-import mutil
+_author = 'Mike Burr'
+_email = 'mburr@unintuitive.com'
+__author__ = '%s <%s>' % (_author, _email)
 
-meta_setup(mutil)
+from distutils.core import setup
+import time
+
+import mutils
+package = mutils
+
+# README.rst dynamically generated:
+with open('README.md', 'w') as f:
+    f.write(package.__doc__)
+
+name = package.__name__
+
+def read(file):
+    with open(file, 'r') as f:
+        return f.read().strip()
+
+setup(
+    name=name,
+    version='0.0.1-%s' % time.time(),
+    long_description=read('README.md'),
+    author=_author,
+    author_email=_email,
+    provides=[name],
+    packages=[name],
+)
